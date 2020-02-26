8c94d491fb45c110b9ae015fde7f783510334c4e
Kayden Clark <kaydenclark725@gmail.com>
2020-02-25 23:50:23 -0700

    now fully checks each field

--- seleniumScript.py
+++ seleniumScript.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome()
-
 test1= ["a@a.a", ""] 
 test2= ["aaa@a.a", "aA1aaaaa"]
 test3= ["a@a.com", "aToast!1"]
@@ -18,15 +16,21 @@ class CreateUser:
         self.password = password
 
     def makeUser(self):
-        driver.set_page_load_timeout(30)
+        driver = webdriver.Chrome()
+        # driver.set_page_load_timeout(30)
         driver.get("https://www.securitymetrics.com/create-account")
-        driver.find_element_by_class_name("create-account-input ng-pristine ng-valid").send_keys(self.comp)
-        driver.find_element_by_class_name("create-account-input ng-pristine ng-valid").send_keys(self.nameF)
-        driver.find_element_by_class_name("create-account-input ng-pristine ng-valid").send_keys(self.nameL)
-        driver.find_element_by_class_name("create-account-input ng-pristine ng-valid").send_keys(self.username)
-        driver.find_element_by_class_name("create-account-input ng-pristine ng-valid").send_keys(self.password)
+        driver.find_element_by_class_name("ng-pristine").send_keys(self.comp)
+        driver.find_element_by_class_name("ng-pristine").send_keys(self.nameF)
+        driver.find_element_by_class_name("ng-pristine").send_keys(self.nameL)
+        driver.find_element_by_class_name("ng-pristine").send_keys(self.username)
+        driver.find_element_by_class_name("ng-pristine").send_keys(self.password)
+        driver.find_element_by_class_name("ng-pristine").send_keys(self.password)
         driver.find_element_by_class_name("create-account-btn").click()
+        driver.get_screenshot_as_file("shoot.png")
         driver.quit()
     
 
-CreateUser("Scrub Co.", "Kayden", "Clark", "Kaydenclark725@gmil.com", "MikeMan8!")
+CreateUser("Scrub Co.", "Kayden", "Clark", test1[0], test1[1]).makeUser()
+# driver = webdriver.Chrome()
+# driver.get("https://www.securitymetrics.com/create-account")
+# driver.find_element_by_class_name("field-container first").send_keys(self.comp)
